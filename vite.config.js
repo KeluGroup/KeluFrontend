a0762e6e9d865be63cbda12e023372c752e01555
Andres <mastro92p@gmail.com>
2026-04-29 18:26:33 +0200

    added vite prerender will be replaced

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vitePrerenderPlugin } from 'vite-prerender-plugin'  // ← new
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    __BRAND_NAME__: JSON.stringify('Kelu'),
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.kelugroup.ch',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [
+    react(),
+    vitePrerenderPlugin({
+      additionalPrerenderRoutes: [
+        '/',
+        '/products',
+        '/about',
+        '/contact',
+        '/faq',
+      ],
+    }),
+  ],
 })
