9e2e3b6f596ee19a03215cdca7663ab5282251d0
Andres <mastro92p@gmail.com>
2026-04-29 18:49:48 +0200

    removed pre render

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePrerenderPlugin } from 'vite-prerender-plugin'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerenderPlugin({
-      prerenderScript: './src/main.jsx',
-      additionalPrerenderRoutes: [
-        '/',
-        '/products',
-        '/about',
-        '/contact',
-        '/faq',
-      ],
-    }),
-  ],
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
-          'vendor-i18n':  ['i18next', 'react-i18next'],
-        },
-      },
-    },
+  plugins: [react()],
+  define: {
+    __BRAND_NAME__: JSON.stringify('Kelu'),
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.kelugroup.ch',
+        changeOrigin: true,
+      }
+    }
+  }
 })
